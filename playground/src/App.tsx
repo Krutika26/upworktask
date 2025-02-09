3ccc81e3db426a3435d86a82ad7e4fa462c0aac9
krutika26 <krutikashahane0@gmail.com>
2025-02-09 15:58:09 +0530

    main

---
+++ playground/src/App.tsx
@@ -0,0 +1,244 @@
+import { useCallback, useState } from 'react'
+
+import RichTextEditor, {
+  Attachment,
+  BaseKit,
+  Blockquote,
+  Bold,
+  BulletList,
+  Clear,
+  Code,
+  CodeBlock,
+  Color,
+  ColumnActionButton,
+  Emoji,
+  Excalidraw,
+  ExportPdf,
+  ExportWord,
+  FontFamily,
+  FontSize,
+  FormatPainter,
+  Heading,
+  Highlight,
+  History,
+  HorizontalRule,
+  Iframe,
+  Image,
+  ImageGif,
+  ImportWord,
+  Indent,
+  Italic,
+  Katex,
+  LineHeight,
+  Link,
+  Mention,
+  Mermaid,
+  MoreMark,
+  OrderedList,
+  SearchAndReplace,
+  SlashCommand,
+  Strike,
+  Table,
+  TableOfContents,
+  TaskList,
+  TextAlign,
+  TextDirection,
+  Twitter,
+  Underline,
+  Video,
+  locale,
+} from 'reactjs-tiptap-editor'
+
+import 'reactjs-tiptap-editor/style.css'
+import 'katex/dist/katex.min.css'
+
+function convertBase64ToBlob(base64: string) {
+  const arr = base64.split(',')
+  const mime = arr[0].match(/:(.*?);/)![1]
+  const bstr = atob(arr[1])
+  let n = bstr.length
+  const u8arr = new Uint8Array(n)
+  while (n--) {
+    u8arr[n] = bstr.charCodeAt(n)
+  }
+  return new Blob([u8arr], { type: mime })
+}
+
+const extensions = [
+  BaseKit.configure({
+    placeholder: {
+      showOnlyCurrent: true,
+    },
+    characterCount: {
+      limit: 50_000,
+    },
+  }),
+  History,
+  SearchAndReplace,
+  TableOfContents,
+  FormatPainter.configure({ spacer: true }),
+  Clear,
+  FontFamily,
+  Heading.configure({ spacer: true }),
+  FontSize,
+  Bold,
+  Italic,
+  Underline,
+  Strike,
+  MoreMark,
+  Katex,
+  Emoji,
+  Color.configure({ spacer: true }),
+  Highlight,
+  BulletList,
+  OrderedList,
+  TextAlign.configure({ types: ['heading', 'paragraph'], spacer: true }),
+  Indent,
+  LineHeight,
+  TaskList.configure({
+    spacer: true,
+    taskItem: {
+      nested: true,
+    },
+  }),
+  Link,
+  Image.configure({
+    upload: (files: File) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(URL.createObjectURL(files))
+        }, 500)
+      })
+    },
+  }),
+  Video.configure({
+    upload: (files: File) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(URL.createObjectURL(files))
+        }, 500)
+      })
+    },
+  }),
+  ImageGif.configure({
+    GIPHY_API_KEY: import.meta.env.VITE_GIPHY_API_KEY as string,
+  }),
+  Blockquote,
+  SlashCommand,
+  HorizontalRule,
+  Code.configure({
+    toolbar: false,
+  }),
+  CodeBlock.configure({ defaultTheme: 'dracula' }),
+  ColumnActionButton,
+  Table,
+  Iframe,
+  ExportPdf.configure({ spacer: true }),
+  ImportWord.configure({
+    upload: (files: File[]) => {
+      const f = files.map(file => ({
+        src: URL.createObjectURL(file),
+        alt: file.name,
+      }))
+      return Promise.resolve(f)
+    },
+  }),
+  ExportWord,
+  Excalidraw,
+  TextDirection,
+  Mention,
+  Attachment.configure({
+    upload: (file: any) => {
+      // fake upload return base 64
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const blob = convertBase64ToBlob(reader.result as string)
+          resolve(URL.createObjectURL(blob))
+        }, 300)
+      })
+    },
+  }),
+  Mermaid.configure({
+    upload: (file: any) => {
+      // fake upload return base 64
+      const reader = new FileReader()
+      reader.readAsDataURL(file)
+
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          const blob = convertBase64ToBlob(reader.result as string)
+          resolve(URL.createObjectURL(blob))
+        }, 300)
+      })
+    },
+  }),
+  Twitter,
+]
+
+const DEFAULT = `<p dir="auto"></p><p dir="auto"></p><p dir="auto"></p><p dir="auto"><div style="text-align: center;" class="image"><img height="auto" style="transform: rotateX(0deg) rotateY(180deg);" src="https://cdn.hashnode.com/res/hashnode/image/upload/v1729198819038/684c0adb-b189-4af8-b9d8-d26e4097ce27.png?auto=compress,format&amp;format=webp" flipx="false" flipy="true" align="center" inline="false"></div></p><p dir="auto"></p><p dir="auto"></p><p dir="auto"></p>`
+
+function debounce(func: any, wait: number) {
+  let timeout: NodeJS.Timeout
+  return function (...args: any[]) {
+    clearTimeout(timeout)
+    // @ts-ignore
+    timeout = setTimeout(() => func.apply(this, args), wait)
+  }
+}
+
+function App() {
+  const [content, setContent] = useState(DEFAULT)
+  const [theme, setTheme] = useState('light')
+  const [disable, setDisable] = useState(false)
+
+  const onValueChange = useCallback(
+    debounce((value: any) => {
+      setContent(value)
+    }, 300),
+    [],
+  )
+  return (
+    <div
+      className="p-[24px] flex flex-col w-full max-w-screen-lg gap-[24px] mx-[auto] my-0"
+      style={{
+        maxWidth: 1024,
+        margin: '40px auto',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          gap: '12px',
+          marginTop: '100px',
+          marginBottom: 10,
+        }}
+      >
+        <button type="button" onClick={() => locale.setLang('vi')}>Hindi</button>
+        <button type="button" onClick={() => locale.setLang('en')}>English</button>
+        <button type="button" onClick={() => locale.setLang('zh_CN')}>French</button>
+        <button type="button" onClick={() => locale.setLang('pt_BR')}>Português</button>
+        <button type="button" onClick={() => locale.setLang('hu_HU')}>Hungarian</button>
+        <button type="button" onClick={() => locale.setLang('hu_HU')}>German</button>
+        <button type="button" onClick={() => locale.setLang('hu_HU')}>Italian</button>
+        <button type="button" onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}>
+          {theme === 'dark' ? 'Light' : 'Dark'}
+        </button>
+        <button type="button" onClick={() => setDisable(!disable)}>{disable ? 'Editable' : 'Readonly'}</button>
+      </div>
+
+      <RichTextEditor
+        output="html"
+        content={content as any}
+        onChangeContent={onValueChange}
+        extensions={extensions}
+        dark={theme === 'dark'}
+        disabled={disable}
+      />
+    </div>
+  )
+}
+
+export default App
